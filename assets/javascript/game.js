9cab210487aef363cb849b1bf62df063297e427d
Kamshat Mussina <mussinak777@gmail.com>
2019-09-09 17:47:05 -0600

    added function in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+
+//var for wins
+//var for losses
+//var for guessesLeft
+//var for youGuessSoFar
+
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessedLetter = [];
+
+// references to HTML
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var guessesLeftText = document.getElementById("guesses-left");
+var youGuessSoFarText = document.getElementById("your-guess-so-far");
+
+
+
+
+document.onkeyup = function (event) {
+  var userGuess = event.key;
+  guessedLetter.push(userGuess);
+
+  var computerGuess = options[Math.floor(Math.random() * options.length)];
+  console.log(computerGuess);
+
+  var restart = function () {
+    guessedLetter = [];
+    guessedLeft  = 9;
+  }
+
+  if (userGuess === computerGuess) {
+
+    wins++;
+  
+    alert("You win!");
+
+  } else if (guessesLeft === 0) {
+    restart();
+    
+
+  } 
+
+  winsText.textContent = "Wins: " + wins;
+  lossesText.textContent = "Losses: " + losses;
+  guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+  youGuessSoFarText.textContent = "Your guess so far: " + guessedLetter;
+
+}
+
